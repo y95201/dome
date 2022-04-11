923c412e626c7bcc67b57bf190e342a0f97d9afe
果果 <31733928+y95201@users.noreply.github.com>
2022-04-11 19:28:07 +0800

    22

--- index.php
+++ index.php
@@ -18,6 +18,6 @@ $config = [
 ];
 
 $llp = new \Y95201\LianLianPay($config);
-// $result = $llp->account->phoneVerifyCodeApply('111','13023667672');  
+// $result = $llp->account->phoneVerifyCodeApply('111','1323667672');  
 $result = $llp->account->phoneVerifyCodeVerify('111','1323667672','123456');  
 print_r($result);die;
